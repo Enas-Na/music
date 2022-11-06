79b46735c4f1faa67571ce4bf7d0d82568390a16
Enas-Na <117593528+Enas-Na@users.noreply.github.com>
2022-11-06 20:13:31 +0200

    Add files via upload

---
+++ redme/music application/src/music/application/Joptionpane.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package music.application;
+
+/**
+ *
+ * @author Enas
+ */
+class Joptionpane {
+
+    static void showMessageDialog(Login aThis, String output) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
---
+++ redme/music application/src/music/application/MusicApplication.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package music.application;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Enas
+ */
+public class MusicApplication {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      // TODO code application logic here
+     JFrame music = new Login();   
+    music.setVisible(true);
+    music.setResizable(false);
+    music.setLocation(100,100);
+    }
+
+    private void setVisable(boolean b) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private void setResizable(boolean b) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private void setLocation(int i, int i0) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
